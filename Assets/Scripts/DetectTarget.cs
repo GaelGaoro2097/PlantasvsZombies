ad47bd80741ded6f7241b9aaf2d3534f133bafa1
GaelGaoro2097 <72483889+GaelGaoro2097@users.noreply.github.com>
2026-09-23 20:36:22 -0600

    Cambios de la clase 23/09
    Cambios

---
+++ Assets/Scripts/DetectTarget.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+ 
+public class DetectTarget : MonoBehaviour
+{
+    [SerializeField]
+    private string targetTag;
+    private float range;
+    [SerializeField]
+    private float rayHeighOffset = 0.5f;
+    private bool isActive;
+    public event System.Action<Health> OnTargetDetected;
+    public void SetRange(float newRange)
+    {
+        range = newRange;
+    }
+    public void SetActive(bool newActive)
+    {
+        isActive = newActive;
+    }
+    private void Update()
+    {
+        if (!isActive) return;
+ 
+        if (Physics.Raycast(transform.position + Vector3.up * rayHeighOffset, transform.forward, out RaycastHit hit, range))
+        {
+            if (hit.collider.CompareTag(targetTag))
+            {
+                OnTargetDetected?.Invoke(hit.collider.GetComponent<Health>());
+            }
+        }
+ 
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(transform.position + Vector3.up * rayHeighOffset, transform.forward * range);
+    }
+}
